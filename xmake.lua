94f0f6f61311d48707975d1688f6711beeff9778
jarari <hazardhax11@gmail.com>
2026-05-27 17:47:13 +0900

    Init

---
+++ xmake.lua
@@ -0,0 +1,39 @@
+-- include subprojects
+includes("lib/commonlibf4")
+
+-- set project constants
+set_project("FO4FasterHdtSMP")
+set_version("0.0.0")
+set_license("GPL-3.0")
+set_languages("c++23")
+set_warnings("allextra")
+
+-- add common rules
+add_rules("mode.debug", "mode.releasedbg")
+add_rules("plugin.vsxmake.autoupdate")
+
+add_defines("COMMONLIB_RUNTIMECOUNT=3")
+if is_mode("debug") then
+    add_requires("bullet3", {configs = {runtimes = "MDd", debug = true}})
+else
+    add_requires("bullet3", {configs = {runtimes = "MD"}})
+end
+add_requires("tbb")
+add_requires("xbyak")
+add_requires("tinyxml2")
+
+-- define targets
+target("FO4FasterHdtSMP")
+    add_rules("commonlibf4.plugin", {
+        name = "FO4FasterHdtSMP",
+        author = "Bingle",
+        description = "Fallout 4 Faster HDT-SMP prototype using CommonLibF4",
+        plugin_template = path.join(os.projectdir(), "res/commonlibf4-plugin.cpp.in"),
+    })
+
+    -- add src files
+    add_files("src/**.cpp")
+    add_headerfiles("src/**.h")
+    add_includedirs("src")
+    add_packages("bullet3", "tbb", "xbyak", "tinyxml2")
+    set_pcxxheader("src/pch.h")
