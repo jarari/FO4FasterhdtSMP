b598e6e7d5eb0f194234c002a4687120ef409752
jarari <hazardhax11@gmail.com>
2026-06-17 16:17:30 +0900

    AE address

--- src/BSBoneMap.h
+++ src/BSBoneMap.h
@@ -6,12 +6,12 @@ namespace Smp
 {
 	inline void RefreshBoneScatterTable(RE::NiAVObject* a_root)
 	{
-		if (!a_root || !REX::FModule::IsRuntimeOG()) {
+		if (!a_root) {
 			return;
 		}
 
 		using Create_t = void (*)(RE::NiAVObject*);
-		static REL::Relocation<Create_t> create{ REL::ID{ 1131947, 0 } };
+		static REL::Relocation<Create_t> create{ REL::ID{ 1131947, 2276147 } };
 		create(a_root);
 	}
 }
